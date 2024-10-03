0caa91989c7f96b0c0587b1fd333aee67bb5cbfb
Krishneshvar <krishjn07@gmail.com>
2024-10-03 13:47:17 +0530

    Home page first UI build after deletion.

--- pylab/src/App.jsx
+++ pylab/src/App.jsx
@@ -1,54 +1,42 @@
-import Navbar from './components/Navbar/Navbar'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <div className="container">
-      <div className="navbar">
-        SAS
+    <main>
+      <div className='site-name'>
+        <h1> SAP </h1>
       </div>
-      <div className="topic">
-        <h1> Students Academic Success </h1>
-        <p>
-          This application allows users to explore various machine learning models for CGPA prediction and classification. It showcases
-          different algorithms, evaluates their performance, and presents insights into feature importance and predictions. Users can upload
-          datasets and select different methods to predict CGPA or classify results based on performance metrics like accuracy, precision,
-          recall, and more.
-        </p>
-      </div>
-      <div className="algorithms">
-        <div className="choices">
-          <h1> Decision Tree </h1>
-          <div className="choice-content">
-            <img src="/decision-tree.png" alt="Decision Tree Logo" />
-            <p></p>
-          </div>
+      <div className="site-home">
+        <div className="site-desc">
+          <h1> Students Academic Performance </h1>
+          <p>
+            This application allows users to explore various machine learning models for CGPA prediction and classification. It showcases
+            different algorithms, evaluates their performance, and presents insights into feature importance and predictions. Users can upload
+            datasets and select different methods to predict CGPA or classify results based on performance metrics like accuracy, precision,
+            recall, and more.
+          </p>
         </div>
-        <div className="choices">
-          <h1> Random Forest </h1>
-          <div className="choice-content">
-            <img src="/random-forest.png" alt="Random Forest Classifier Logo" />
-            <p></p>
+        <div className="algorithms">
+          <div className="cards">
+            ;
           </div>
-        </div>
-        <div className="choices">
-          <h1> SVC </h1>
-          <div className="choice-content">
-            <img src="/support-vector.png" alt="Support Vector Classifier Logo" />
-            <p></p>
+          <div className="cards">
+            ;
           </div>
-        </div>
-        <div className="choices">
-          <h1> ID3 </h1>
-          <div className="choice-content">
-            <img src="/decision-tree.png" alt="ID3 Logo" />
-            <p></p>
+          <div className="cards">
+            ;
+          </div>
+          <div className="cards">
+            ;
           </div>
         </div>
       </div>
-    </div>
+      <div className="footer">
+        Hello
+      </div>
+    </main>
     </>
   )
 }
